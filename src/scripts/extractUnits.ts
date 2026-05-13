bf07ed3c73e35322ffdb82616d7172f24f358be4
aloisclerc <alois.clerc@gmail.com>
2026-05-13 12:13:00 -0400

    aeldari units works

--- src/scripts/extractUnits.ts
+++ src/scripts/extractUnits.ts
@@ -18,6 +18,13 @@ const builder = new XMLBuilder({
 
 const DATA_PATH = "../wh40k-10e"
 
+const factionObj = {
+  "Space Marines": [],
+  "Imperium": [],
+  "Chaos": [],
+  "Xenos": [['Aeldari', 'Aeldari - Aeldari Library.cat']]
+}
+
 const outputJSON: Unit[]  = [];
 
 function readAllCatFiles(dir: string): any[] {
@@ -38,7 +45,7 @@ function readCatFile(dir: string, fileName: string): any[] {
   return parser.parse(xml);
 }
 
-function walk(json: any) {
+function walk(json: any, faction: string) {
   
   const results: string[] = []
 
@@ -61,13 +68,7 @@ function walk(json: any) {
         for (const entry of entries) {
           if (entry["@_type"] === "unit") {
             //build json here
-            console.log(entry);
-            createUnit(entry);
-            return
-            const xmlFragment = builder.build({
-              selectionEntry: entry,
-            })
-            results.push(xmlFragment)
+            createUnit(entry, faction);
           }
 
           stack.push(entry)
@@ -77,18 +78,40 @@ function walk(json: any) {
       }
     }
   }
-  console.log(results.length)
-  console.log(results[0])
-  return results
 }
 
-function createUnit(entry: any){
-const entryJSON: Unit = {"name": "", "faction": "", "role": "", "points": 0};
-entry["costs"]["cost"].forEach(element => {
-  if(element["@_name"] === "pts") entryJSON["points"] = element["@_value"]
-});
+function createUnit(entry: any, faction: string){
+  const entryJSON: Unit = {"name": "", "faction": "", "role": "", "points": 0};
+  entryJSON["name"] = entry["@_name"];
+  entryJSON["faction"] = faction;
+  
+    entry["categoryLinks"]["categoryLink"].forEach(element => {
+      if(element["@_primary"] === "true") entryJSON["role"] = element["@_name"]
+      if(element["@_name"] === 'Faction: Drukhari') entryJSON["faction"] = "Drukhari"
+    });
+    try{
+    if(typeof entry["costs"] !== 'undefined'){
+      if(Array.isArray(entry["costs"]["cost"])){
+        entry["costs"]["cost"].forEach(element => {
+          if(element["@_name"] === "pts") entryJSON["points"] = element["@_value"]
+        });
+      } else {
+          if(entry["costs"]["cost"]["@_name"] === "pts") entryJSON["points"] = entry["costs"]["cost"]["@_value"]
+      }
+    } else if(typeof entry["selectionEntries"] !== undefined){
+      entry["selectionEntries"]["selectionEntry"]["costs"]["cost"].forEach(element => {
+          if(element["@_name"] === "pts") entryJSON["points"] = element["@_value"]
+      });
+    } else {
+      console.log("ERROR");
+      console.log(entry);
+    }
+    } catch {
+      console.log("ERROR");
+      console.log(entry["costs"]["cost"])
+    }   
 
-outputJSON.push(entryJSON);
+  outputJSON.push(entryJSON);
 
 }
 
@@ -97,9 +120,10 @@ outputJSON.push(entryJSON);
 
 function main() {
   const data = readCatFile(DATA_PATH, 'Aeldari - Aeldari Library.cat');
-  const justUnits = walk(data)
+  walk(data, 'Aeldari')
 
-  console.log(outputJSON);
+  const json = JSON.stringify(outputJSON, null, 2)
+  fs.writeFileSync("./src/data/units.json", json, "utf-8");
 
 }
 
